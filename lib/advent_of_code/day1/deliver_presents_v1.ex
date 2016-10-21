62cb3105c24eb5136814645b19d3046b0069e645
Rafael Rocha <rafaelrochasilva@gmail.com>
2016-10-21 18:06:42 -0200

    Add moduledoc for Deliver Present

--- lib/advent_of_code/day1/deliver_presents_v1.ex
+++ lib/advent_of_code/day1/deliver_presents_v1.ex
@@ -1,4 +1,19 @@
 defmodule AdventOfCode.Day1.DeliverPresentsV1 do
+  @moduledoc """
+  Deliver Presents is used to help Santa to find the proper floor.
+  """
+
+  @doc """
+  Defines the floor that Santa should deliver the present.
+
+  Returns 'floor'
+
+  ## Examples
+    iex> AdventOfCode.Day1.DeliverPresentsV1.floor("(())")
+    0
+    iex> AdventOfCode.Day1.DeliverPresentsV1.floor("(())")
+    0
+  """
   def floor(deliver) do
     deliver
     |> to_char_list
